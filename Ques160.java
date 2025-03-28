7f4fb3830bf7ecef5b78fd577463317d7ffcc069
Shivaji1408 <kumarshivaji0837@gmail.com>
2025-03-28 08:55:52 +0530

    Update files

--- Ques160.java
+++ Ques160.java
@@ -1,20 +1,40 @@
-import java.lang.classfile.components.ClassPrinter.ListNode;
-
 public class Ques160 {
+
+    // Make ListNode static to be used in the static method
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
     public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         ListNode a = headA;
         ListNode b = headB;
 
-        while(a != b){
-            a= a==null ? headB:a.next;
-            b= b==null ? headA:b.next;
+        while (a != b) {
+            a = (a == null) ? headB : a.next;
+            b = (b == null) ? headA : b.next;
         }
         return a;
-
     }
-    public static void main(String args[]){
-        int headA = 12;
-        int headB = 13;
-        getIntersectionNode(headA,headB);
+
+    public static void main(String args[]) {
+        // Create example linked lists
+        ListNode headA = new ListNode(12);
+        ListNode headB = new ListNode(13);
+
+        // Call the method correctly
+        ListNode intersection = getIntersectionNode(headA, headB);
+
+        // Print the result
+        if (intersection != null) {
+            System.out.println("Intersection at node with value: " + intersection.val);
+        } else {
+            System.out.println("No intersection found.");
+        }
     }
 }
--- Ques876.java
+++ Ques876.java
@@ -1,10 +1,19 @@
-import java.lang.classfile.components.ClassPrinter.ListNode;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
 
 public class Ques876 {
     public ListNode middleNode(ListNode head) {
         ListNode slow = head;
         ListNode fast = head;
-        while(fast != null && fast.next != null){
+        while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
