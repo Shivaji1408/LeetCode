76a41733c76036ac49077424d0d29f99d41a9766
Shivaji1408 <kumarshivaji0837@gmail.com>
2025-04-07 08:36:04 +0530

    Some changes

--- Ques14.java
+++ Ques14.java
@@ -1,6 +1,3 @@
-
-import java.util.Arrays;
-
 // Write a function to find the longest common prefix string amongst an array of strings.
 // If there is no common prefix, return an empty string "".
 
@@ -12,6 +9,9 @@ import java.util.Arrays;
 // Input: strs = ["dog","racecar","car"]
 // Output: ""
 // Explanation: There is no common prefix among the input strings.
+
+
+import java.util.Arrays;
 
 public class Ques14 {
     public static String longestcommonprefix(String[] s){
