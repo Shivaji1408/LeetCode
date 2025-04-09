18651965f87020f8494ef58105ae0bb14302406a
Shivaji1408 <kumarshivaji0837@gmail.com>
2025-04-09 22:55:52 +0530

    Question 242 Solution LeetCode

---
+++ Ques242.java
@@ -0,0 +1,31 @@
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+// Example 1:
+// Input: s = "anagram", t = "nagaram"
+// Output: true
+
+// Example 2:
+// Input: s = "rat", t = "car"
+// Output: false
+
+// Constraints:
+// 1 <= s.length, t.length <= 5 * 104
+// s and t consist of lowercase English letters.
+
+import java.util.*;
+
+public class Ques242 {
+    public static boolean isAnagram(String s, String t) {
+        char[] arr1 = s.toCharArray();
+        char[] arr2 = t.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        return Arrays.equals(arr1, arr2);
+    }
+
+    public static void main(String args[]){
+        String s = "anagram";
+        String t = "nagaram";
+        System.out.println(isAnagram(s,t));
+    }
+}
