32cbe4100e859e303f02b0e225b4d58736c496b0
Shivaji1408 <kumarshivaji0837@gmail.com>
2025-04-22 23:59:32 +0530

    Add Question

--- Ques350.java
+++ Ques350.java
@@ -1,5 +1,21 @@
-public class Solution {
-    public int[] intersect(int[] nums1, int[] nums2) {
+// Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+
+// Example 1:
+// Input: nums1 = [1,2,2,1], nums2 = [2,2]
+// Output: [2,2]
+
+// Example 2:
+// Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+// Output: [4,9]
+// Explanation: [9,4] is also accepted.
+ 
+// Constraints:
+// 1 <= nums1.length, nums2.length <= 1000
+// 0 <= nums1[i], nums2[i] <= 1000
+
+import java.util.*;
+public class Ques350 {
+    public static int[] intersect(int[] nums1, int[] nums2) {
         Arrays.sort(nums1);
         Arrays.sort(nums2);
         ArrayList<Integer> arr = new ArrayList<Integer>();
@@ -24,5 +40,16 @@ public class Solution {
             k++;
         }
         return output;
+    }
+
+    public static void printarr(int[] arr){
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i] + " ");
+        }
+    }
+    public static void main(String args[]){
+        int[] nums1 = {1,2,2,1};
+        int[] nums2 = {2,2};
+        printarr(intersect(nums1, nums2));
     }
 }
