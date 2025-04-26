1f529fbbadd3186d1264d69580c83e3c623fe54c
Shivaji1408 <kumarshivaji0837@gmail.com>
2025-04-26 20:44:38 +0530

    Add Question

--- Ques680.java
+++ Ques680.java
@@ -1,4 +1,24 @@
-class Ques680 {
+// Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+
+// Example 1:
+// Input: s = "aba"
+// Output: true
+
+// Example 2:
+// Input: s = "abca"
+// Output: true
+// Explanation: You could delete the character 'c'.
+
+// Example 3:
+// Input: s = "abc"
+// Output: false
+
+// Constraints:
+// 1 <= s.length <= 105
+// s consists of lowercase English letters.
+
+
+public class Ques680 {
     public static boolean validPalindrome(String s) {
         int left = 0, right = s.length() - 1;
         
