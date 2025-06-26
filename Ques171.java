4d5b700f09941de7c477e142aef6705f4e5265fb
Shivaji1408 <kumarshivaji0837@gmail.com>
2025-06-26 21:39:05 +0530

    Ques171 Solution

---
+++ Ques171.java
@@ -0,0 +1,13 @@
+public class Ques171 {
+    public static int titleToNumber(String columnTitle) {
+        int result = 0;
+        for (int i = 0; i < columnTitle.length(); i++) {
+            result = result * 26 + (columnTitle.charAt(i) - 'A' + 1);
+        }
+        return result;
+    }
+    public static void main(String args[]){
+        String columnTitle = "AA";
+        System.out.println(titleToNumber(columnTitle));
+    }
+}
