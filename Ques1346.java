7a6f21966bc395b3aca523d7e57716351f6ce075
Shivaji1408 <kumarshivaji0837@gmail.com>
2025-07-06 10:59:35 +0530

    Question 1346 Solution Lettcode

---
+++ Ques1346.java
@@ -0,0 +1,16 @@
+public class Ques1346 {
+    public static boolean checkIfExist(int[] arr) {
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr.length; j++){
+                if(arr[i] == 2*arr[j] && i != j){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int[] arr = {10,2,5,3};
+        System.out.println(checkIfExist(arr));
+    }
+}
