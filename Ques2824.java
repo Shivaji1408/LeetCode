59a55fd14bcadc93d507d58896577f0be57e9b2c
Shivaji1408 <kumarshivaji0837@gmail.com>
2025-07-13 19:52:24 +0530

    Question 2824 Solution

---
+++ Ques2824.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Ques2824 {
+    public static int countPairs(List<Integer> nums, int target) {
+        int count = 0;
+        for(int i=0; i<nums.size(); i++){
+            for(int j=i+1; j<nums.size(); j++){
+                if((nums.get(i) + nums.get(j)) < target){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        List<Integer> nums = Arrays.asList(-1,1,2,3,1);
+        int target = 2;
+        System.out.println(countPairs(nums,target));
+    }
+}
