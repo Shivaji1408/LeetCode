303e147be97dc8e1085978bfa6f4feb584d54f38
Shivaji1408 <kumarshivaji0837@gmail.com>
2025-08-20 23:06:08 +0530

    Solution practice

--- Solution.java
+++ Solution.java
@@ -1,10 +1,23 @@
 import java.util.Scanner;
 public class Solution {
+    public static int[] findFactors(int number){
+        int count = 0;
+        int[] arr = new int[number];
+        for(int i=1; i<number; i++){
+            if(number % i == 0){
+                arr[count] = i;
+                count++;
+            }
+        }
+        return arr;
+    }
+
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         int number = sc.nextInt();
-        for(int i=1; i<11; i++){
-            System.out.println(number + " * " + i + " = " + (number*i));
+        
+        for(int i=1; i<=findFactors(number).length; i++){
+            System.out.println(findFactors(number)[i]);
         }
     }
 }
