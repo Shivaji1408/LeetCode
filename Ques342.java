58f0bfb2462450d22fecdb8fb1041f068ef16fad
Shivaji1408 <kumarshivaji0837@gmail.com>
2025-08-24 20:02:53 +0530

    Ques 342 Recursive Solution

---
+++ Ques342.java
@@ -0,0 +1,18 @@
+public class Ques342{
+    public static boolean isPowerOfFour(int n) {
+        if(n<=0){
+            return false;
+        }
+        if(n==1){
+            return true;
+        }
+        if(n%4 != 0){
+            return false;
+        }
+        return isPowerOfFour(n/4);
+    }
+    public static void main(String args[]){
+        int n= 4;
+        System.out.println(n);
+    }
+}
