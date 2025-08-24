2e02ba384c3aa65f3d4210858786a3a3b4168b67
Shivaji1408 <kumarshivaji0837@gmail.com>
2025-08-24 19:51:49 +0530

    Ques 326 Solution

---
+++ Ques326.java
@@ -0,0 +1,19 @@
+public class Ques326 {
+    public static boolean isPowerOfThree(int n) {
+        if(n<=0){
+            return false;
+        }
+        if(n==1){
+            return true;
+        }
+        if(n%3 != 0){
+            return false;
+        }
+        return isPowerOfThree(n/3);
+    }
+
+    public static void main(String args[]){
+        int n=27;
+        System.out.println(isPowerOfThree(n));
+    }
+}
