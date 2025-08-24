c9f9957eb6cf7e6fba5d2446884f614845bae067
Shivaji1408 <kumarshivaji0837@gmail.com>
2025-08-24 19:56:03 +0530

    Two Approach to solve this question

--- Ques326.java
+++ Ques326.java
@@ -1,15 +1,21 @@
 public class Ques326 {
     public static boolean isPowerOfThree(int n) {
-        if(n<=0){
-            return false;
-        }
-        if(n==1){
-            return true;
-        }
-        if(n%3 != 0){
-            return false;
-        }
-        return isPowerOfThree(n/3);
+        // Recursive Method
+        
+        // if(n<=0){
+        //     return false;
+        // }
+        // if(n==1){
+        //     return true;
+        // }
+        // if(n%3 != 0){
+        //     return false;
+        // }
+        // return isPowerOfThree(n/3);
+
+        // Direct Solution
+        int maxPowerOf3 = 1162261467; // 3^19 is the largest power of 3 in int range
+        return n > 0 && maxPowerOf3 % n == 0;
     }
 
     public static void main(String args[]){
