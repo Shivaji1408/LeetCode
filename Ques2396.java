feebe787071637310c89033fab1a4fb9fc9a1dd4
Shivaji1408 <kumarshivaji0837@gmail.com>
2025-08-27 00:06:49 +0530

    Ques 2396 Solution

---
+++ Ques2396.java
@@ -0,0 +1,10 @@
+public class Ques2396 {
+    public static boolean isStrictlyPalindromic(int n) {
+        return false;
+    }
+
+    public static void main(String args[]){
+        int n = 9;
+        System.out.println(isStrictlyPalindromic(n));
+    }
+}
