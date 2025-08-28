19f31698c13967242933fcfed86659fb6a006a35
Shivaji1408 <kumarshivaji0837@gmail.com>
2025-08-28 10:13:35 +0530

    Ques 202 Solution

---
+++ Ques202.java
@@ -0,0 +1,19 @@
+public class Ques202{
+    public static boolean isHappy(int n) {
+        if(n==1 || n==7) return true;
+        else if(n<10) return false;
+        else{
+            int sum=0;
+            while(n>0){
+                int temp=n%10;
+                sum+= temp*temp;
+                n=n/10;
+            }
+            return isHappy(sum);
+        }
+    }
+    public static void main(String args[]){
+        int n= 1000;
+        System.out.println(isHappy(n));
+    }
+}
