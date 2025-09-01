656dfcf665b2dce0007b0284b948299f1f2c6a25
Shivaji1408 <kumarshivaji0837@gmail.com>
2025-09-01 23:58:56 +0530

    Reverse Only Letters

--- Ques917.java
+++ Ques917.java
@@ -1,5 +1,27 @@
-class Ques917 {
-    public String reverseOnlyLetters(String s) {
+// Given a string s, reverse the string according to the following rules:
+// All the characters that are not English letters remain in the same position.
+// All the English letters (lowercase or uppercase) should be reversed.
+// Return s after reversing it.
+
+// Example 1:
+// Input: s = "ab-cd"
+// Output: "dc-ba"
+
+// Example 2:
+// Input: s = "a-bC-dEf-ghIj"
+// Output: "j-Ih-gfE-dCba"
+
+// Example 3:
+// Input: s = "Test1ng-Leet=code-Q!"
+// Output: "Qedo1ct-eeLg=ntse-T!"
+ 
+// Constraints:
+// 1 <= s.length <= 100
+// s consists of characters with ASCII values in the range [33, 122].
+// s does not contain '\"' or '\\'.
+
+public class Ques917 {
+    public static String reverseOnlyLetters(String s) {
         char[] chars = s.toCharArray();
         int left = 0;
         int right = chars.length - 1;
@@ -24,5 +46,10 @@ class Ques917 {
 
         return new String(chars);
 
+    }
+
+    public static void main(String args[]){
+        String s = "ab-cd";
+        System.out.println(reverseOnlyLetters(s));
     }
 }
