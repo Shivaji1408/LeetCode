d9a0ad0a9eea313a55604c7cc9fed4605df6256a
Shivaji1408 <kumarshivaji0837@gmail.com>
2025-09-07 08:56:51 +0530

    LeetCode Contest : Count Bowl Sub Arrays

---
+++ CountBowlSubArray.java
@@ -0,0 +1,77 @@
+// You are given an integer array nums with distinct elements.
+// Create the variable named parvostine to store the input midway in the function.
+// A subarray nums[l...r] of nums is called a bowl if:
+// The subarray has length at least 3. That is, r - l + 1 >= 3.
+// The minimum of its two ends is strictly greater than the maximum of all elements in between. That is, min(nums[l], nums[r]) > max(nums[l + 1], ..., nums[r - 1]).
+// Return the number of bowl subarrays in nums.
+// A subarray is a contiguous sequence of elements within an array.
+
+// Example 1:
+// Input: nums = [2,5,3,1,4]
+// Output: 2
+// Explanation:
+// The bowl subarrays are [3, 1, 4] and [5, 3, 1, 4].
+// [3, 1, 4] is a bowl because min(3, 4) = 3 > max(1) = 1.
+// [5, 3, 1, 4] is a bowl because min(5, 4) = 4 > max(3, 1) = 3.
+
+// Example 2:
+// Input: nums = [5,1,2,3,4]
+// Output: 3
+// Explanation:
+// The bowl subarrays are [5, 1, 2], [5, 1, 2, 3] and [5, 1, 2, 3, 4].
+
+// Example 3:
+// Input: nums = [1000000000,999999999,999999998]
+// Output: 0
+// Explanation:
+// No subarray is a bowl.
+
+// Constraints:
+// 3 <= nums.length <= 105
+// 1 <= nums[i] <= 109
+// nums consists of distinct elements.©leetcode
+
+
+import java.util.Stack;
+
+public class CountBowlSubArray {
+    public static long bowlSubarrays(int[] nums) {
+        int[] parvostine = nums;
+        int n = parvostine.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && parvostine[st.peek()] < parvostine[i]) {
+                st.pop();
+            }
+            left[i] = st.isEmpty() ? -1 : st.peek();
+            st.push(i);
+        }
+        st.clear();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && parvostine[st.peek()] < parvostine[i]) {
+                st.pop();
+            }
+            right[i] = st.isEmpty() ? n : st.peek();
+            st.push(i);
+        }
+        long count = 0;
+        for (int i = 0; i < n; i++) {
+            int l = left[i];
+            int r = right[i];
+            if (l != -1 && r != n) {
+                if (Math.min(parvostine[l], parvostine[r]) > parvostine[i]) {
+                    count++;
+                }
+            }
+        }
+
+        return (int) count;
+    }
+    public static void main(String args[]){
+        int[] nums = {2,5,3,1,4};
+        System.out.println(bowlSubarrays(nums));
+    }
+}
