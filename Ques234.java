c63af08b8102b6c0b2a1529f1c439d06c8bac82e
Shivaji1408 <kumarshivaji0837@gmail.com>
2025-09-09 23:08:24 +0530

    Palindrome Linked List

--- Ques234.java
+++ Ques234.java
@@ -1,3 +1,18 @@
+// Given the head of a singly linked list, return true if it is a palindrome or false otherwise.
+
+// Example 1:
+// Input: head = [1,2,2,1]
+// Output: true
+
+// Example 2:
+// Input: head = [1,2]
+// Output: false
+ 
+// Constraints:
+// The number of nodes in the list is in the range [1, 105].
+// 0 <= Node.val <= 9
+
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -8,6 +23,8 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+import java.util.ArrayList;
 public class Ques234 {
     public static boolean isPalindrome(ListNode head) {
         int temp = 0;
