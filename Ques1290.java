f9b8c7a886428f13c8a49cd5bd4303ba3af2b3b1
Shivaji1408 <kumarshivaji0837@gmail.com>
2025-09-10 09:47:56 +0530

    Convert Binary Number in a Linked List to Integer

--- Ques1290.java
+++ Ques1290.java
@@ -1,15 +1,32 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+// Given head which is a reference node to a singly-linked list. 
+// The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
+// Return the decimal value of the number in the linked list.
+// The most significant bit is at the head of the linked list.
+
+// Example 1:
+// Input: head = [1,0,1]
+// Output: 5
+// Explanation: (101) in base 2 = (5) in base 10
+
+// Example 2:
+// Input: head = [0]
+// Output: 0
+
+// Constraints:
+// The Linked List is not empty.
+// Number of nodes will not exceed 30.
+// Each node's value is either 0 or 1.
+
 import java.util.ArrayList;
-public class Solution {
+
+public class Ques1290 {
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
     public static int getDecimalValue(ListNode head) {
         ArrayList<Integer> lst = new ArrayList<>();
         while(head != null){
@@ -24,5 +41,12 @@ public class Solution {
             a++;
         }
         return res;
+    }
+    public static void main(String[] args) {
+        ListNode head = new Ques1290().new ListNode(1);
+        head.next = new Ques1290().new ListNode(0);
+        head.next.next = new Ques1290().new ListNode(1);
+        int result = getDecimalValue(head);
+        System.out.println(result);
     }
 }
