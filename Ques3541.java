5065c4019cb1bffb8445e1068fe078cd60d1c460
Shivaji1408 <kumarshivaji0837@gmail.com>
2025-09-13 19:22:05 +0530

    Ques3541 Solution

---
+++ Ques3541.java
@@ -0,0 +1,19 @@
+public class Ques3541 {
+    public static int maxFreqSum(String s) {
+        int[] freq = new int[26];
+        int maxVowel = 0, maxConso = 0;
+        for (char c : s.toCharArray()) {
+            int i = c - 'a';
+            freq[i]++;
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+                maxVowel = Math.max(maxVowel, freq[i]);
+            else
+                maxConso = Math.max(maxConso, freq[i]);
+        }
+        return maxVowel + maxConso;
+    }
+    public static void main(String args[]){
+        String str = "success";
+        System.out.println(maxFreqSum(str));
+    }
+}
