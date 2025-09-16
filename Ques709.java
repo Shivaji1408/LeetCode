b728c563f28bffa28e16a7b3266bfa70d7f2b7d3
Shivaji1408 <kumarshivaji0837@gmail.com>
2025-09-16 12:16:16 +0530

    Ques 709 Solution

---
+++ Ques709.java
@@ -0,0 +1,9 @@
+public class Ques709 {
+    public static String toLowerCase(String s) {
+        return s.toLowerCase();
+    }
+    public static void main(String args[]){
+        String str = "HELLO";
+        System.out.println(toLowerCase(str));
+    }
+}
