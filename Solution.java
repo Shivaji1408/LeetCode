b789532406c02ac3fb652d4657b3455de23decf5
Shivaji1408 <kumarshivaji0837@gmail.com>
2025-09-17 11:12:51 +0530

    Clear Digits

--- Solution.java
+++ Solution.java
@@ -1,16 +1,23 @@
 public class Solution {
-    public class Student{
+    abstract class Person {
+        abstract void display();
+    }
+    static class Student extends Person {
         String name;
         int age;
 
-        Student(String name, int age){
+        Student(String name, int age) {
             this.name = name;
             this.age = age;
+        }
+
+        void display()  {
+            System.out.println("Name: " + name + ", Age: " + age);
         }
     }
     
     public static void main(String args[]){
         Student student1 = new Student();
-        System.out.println();
+        student1.display();
     }
 }
