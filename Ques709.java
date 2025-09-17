089bab7a8ea4adb4cde65ca8c3154c7ee8a1666b
Shivaji1408 <kumarshivaji0837@gmail.com>
2025-09-17 08:59:29 +0530

    To Lower Case (Three Different Solution)

--- Ques709.java
+++ Ques709.java
@@ -19,7 +19,31 @@
 
 public class Ques709 {
     public static String toLowerCase(String s) {
-        return s.toLowerCase();
+        // ===>> First Easy Solution <<===
+        
+        // return s.toLowerCase();
+        
+        // ===>> Second Solution <<===
+        StringBuilder str = new StringBuilder();
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) >= 'A' && s.charAt(i) <= 'Z'){
+                str.append((char)(s.charAt(i) + 32));
+            }else{
+                str.append(s.charAt(i));
+            }
+        }
+        return str.toString();
+        
+        // ===>> Third Solution <<===
+        // String str = "";
+        // for(int i=0; i<s.length(); i++){
+        //     if(s.charAt(i) >= 'A' && s.charAt(i) <= 'Z'){
+        //         str += (char)(s.charAt(i) + 32);
+        //     }else{
+        //         str += s.charAt(i);
+        //     }
+        // }
+        // return str;
     }
     public static void main(String args[]){
         String str = "HELLO";
