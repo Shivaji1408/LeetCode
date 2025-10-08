03aa6c817ab0ce67c0d287befe6f26d7d02641bf
Shivaji1408 <kumarshivaji0837@gmail.com>
2025-10-08 08:20:16 +0530

    Practice Work

--- Solution.java
+++ Solution.java
@@ -1,8 +1,19 @@
+import java.util.*;
+
 public class Solution {
     public static void main(String args[]){
-        int n = 50;
-        if(n % 2 == 0){
-            System.out.println(n + " is a Even Number");
+        int n = 3;
+        ArrayList<Integer[]> lst = new ArrayList<>();
+        // max(i - loc[i],1) to min(i + loc[i],n)
+        int[] loc = {1,1,1};
+        for(int i=1; i<n+1; i++){
+            int a = Math.max( (i-loc[i-1]) , 1);
+            int b = Math.min( (i+loc[i-1]) , n);
+            lst.add({a,b});
+        }
+
+        for(int i=0; i<lst.size(); i++){
+            
         }
     }
 }
