0b2710fd2a38d375da1f7750ed1912ab741d8b44
Shiv Kumar <kumarshivaji0837@gmail.com>
2025-11-01 18:42:45 +0530

    Reshape The Matrix

--- Ques566.java
+++ Ques566.java
@@ -34,18 +34,22 @@ public class Ques566 {
     }
 
     public static int[][] matrixReshape(int[][] mat, int r, int c) {
+
         if((r*c) != (mat.length*mat[0].length)){
             return mat;
         }
+
         int[] temp = oneDArray(mat);
         int count = 0;
         int[][] res = new int[r][c];
+
         for(int i=0; i<r; i++){
             for(int j=0; j<c; j++){
                 res[i][j] = temp[count];
                 count++;
             }
         }
+
         return res;
     }
 
@@ -53,11 +57,13 @@ public class Ques566 {
         int[][] mat = {{1,2},{3,4}};
         int r = 1, c = 4;
         int[][] ans = matrixReshape(mat, r, c);
+
         for(int i=0; i<ans.length; i++){
             for(int j=0; j<ans[0].length; j++){
                 System.out.print(ans[i][j] + " ");
             }
             System.out.println();
         }
+        
     }
 }
