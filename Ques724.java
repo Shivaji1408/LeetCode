a5d0509ce80c416af808dc0eecd7278ed2558e3b
Shiv Kumar <kumarshivaji0837@gmail.com>
2025-11-04 22:42:45 +0530

    Find Pivot Index

--- Ques724.java
+++ Ques724.java
@@ -34,11 +34,15 @@ public class Ques724 {
     public static int pivotIndex(int[] nums) {
         int totalSum = 0;
         int leftsum = 0;
-        for (int ele : nums)
+
+        for (int ele : nums){
             totalSum += ele;
-        for (int i = 0; i < nums.length; leftsum += nums[i++])
-            if (leftsum * 2 == totalSum - nums[i])
+        }
+        for (int i = 0; i < nums.length; leftsum += nums[i++]){
+            if (leftsum * 2 == totalSum - nums[i]){
                 return i;
+            }
+        }
         return -1; 
     }
 
