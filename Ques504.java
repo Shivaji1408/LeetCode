461d4d5dd8f30ba31a4b2dde8f6d14d99cc38920
Shivaji1408 <kumarshivaji0837@gmail.com>
2025-11-05 23:17:09 +0530

    Base 7

--- Ques504.java
+++ Ques504.java
@@ -1,3 +1,17 @@
+// Given an integer num, return a string of its base 7 representation.
+
+// Example 1:
+// Input: num = 100
+// Output: "202"
+
+// Example 2:
+// Input: num = -7
+// Output: "-10"
+ 
+// Constraints:
+// -107 <= num <= 107
+
+
 public class Solution {
     public static String convertToBase7(int num) {
         if(num == 0){
