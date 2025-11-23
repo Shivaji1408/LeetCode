db646839ed86e08618deb34f941114e8a3157112
Shiv Kumar <kumarshivaji0837@gmail.com>
2025-11-23 15:20:24 +0530

    Ques 215 Solution

---
+++ Ques215.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Solution {
+    public static int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (int i = 0; i < k; i++) {
+            minHeap.offer(nums[i]);
+        }
+        for (int i = k; i < nums.length; i++) {
+            if (nums[i] > minHeap.peek()) {
+                minHeap.poll();
+                minHeap.offer(nums[i]);
+            }
+        }
+        return minHeap.peek();
+    }
+
+    public static void main(String args[]){
+        int[] nums = {99,99};
+        int k = 1;
+        System.out.println(findKthLargest(nums, k));
+    }
+}
