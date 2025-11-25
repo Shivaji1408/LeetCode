1d12775008e24f4dd4f721d75efbfab8fa98086b
Shiv Kumar <kumarshivaji0837@gmail.com>
2025-11-25 18:34:31 +0530

    Ques 1015 Solution

---
+++ Ques1015.java
@@ -0,0 +1,20 @@
+public class Ques1015 {
+    public static int smallestRepunitDivByK(int k) {
+        if(k%2 == 0 || k%5 == 0){
+            return -1;
+        }
+        int rem = 0;
+        for(int i=1; i<=k; i++){
+            rem = (rem*10 + 1) % k;
+            if(rem == 0){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String args[]){
+        int k = 3;
+        System.out.println(smallestRepunitDivByK(k));
+    }
+}
