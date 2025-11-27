55dbeeef18b15e10971a2fe6e54db2177d834cb5
Shiv Kumar <kumarshivaji0837@gmail.com>
2025-11-27 22:22:56 +0530

    Valid Boomerang

--- Ques1037.java
+++ Ques1037.java
@@ -1,3 +1,20 @@
+// Given an array points where points[i] = [xi, yi] represents a point on the X-Y plane, return true if these points are a boomerang.
+// A boomerang is a set of three points that are all distinct and not in a straight line.
+
+// Example 1:
+// Input: points = [[1,1],[2,3],[3,2]]
+// Output: true
+
+// Example 2:
+// Input: points = [[1,1],[2,2],[3,3]]
+// Output: false
+ 
+// Constraints:
+// points.length == 3
+// points[i].length == 2
+// 0 <= xi, yi <= 100
+
+
 public class Ques1037{
     public static boolean isBoomerang(int[][] points) {
         int x1 = points[0][0], y1 = points[0][1];
