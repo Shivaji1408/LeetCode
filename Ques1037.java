4dcd106cb7fc5a9ef443a4f9e818803a91c4b07c
Shiv Kumar <kumarshivaji0837@gmail.com>
2025-11-27 22:19:19 +0530

    Ques 1037 Solution

---
+++ Ques1037.java
@@ -0,0 +1,14 @@
+public class Ques1037{
+    public static boolean isBoomerang(int[][] points) {
+        int x1 = points[0][0], y1 = points[0][1];
+        int x2 = points[1][0], y2 = points[1][1];
+        int x3 = points[2][0], y3 = points[2][1];
+
+        return (y2 - y1) * (x3 - x2) != (y3 - y2) * (x2 - x1);
+    }
+
+    public static void main(String args[]){
+        int[][] points = {{1,1},{2,2},{3,3}};
+        System.out.println(isBoomerang(points));
+    }
+}
