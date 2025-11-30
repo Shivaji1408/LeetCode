87824581c5fd4b777558c268ee1f2e3d89971709
Shiv Kumar <kumarshivaji0837@gmail.com>
2025-11-30 19:17:03 +0530

    Types of Triangle

--- Ques3024.java
+++ Ques3024.java
@@ -23,24 +23,46 @@
 // nums.length == 3
 // 1 <= nums[i] <= 100 
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Ques3024 {
     public static String triangleType(int[] nums) {
-        int a = nums[0];
-        int b = nums[1];
-        int c = nums[2];
-        if (a + b <= c || a + c <= b || b + c <= a) {
+        // Solutin Using Set
+        Arrays.sort(nums);
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums){ 
+            set.add(num);
+        }
+        if (nums[0] + nums[1] <= nums[2]){ 
             return "none";
         }
-        if (a == b && b == c) {
+        if (set.size() == 1){ 
             return "equilateral";
-        } 
-        else if (a == b || b == c || a == c) {
-            return "isosceles";
-        } 
-        else {
-            return "scalene";
         }
+        if (set.size() == 2){ 
+            return "isosceles";
+        }
+       return "scalene";
+        
+        // Easy Solution
+        
+        // int a = nums[0];
+        // int b = nums[1];
+        // int c = nums[2];
+        // if (a + b <= c || a + c <= b || b + c <= a) {
+        //     return "none";
+        // }
+        // if (a == b && b == c) {
+        //     return "equilateral";
+        // } 
+        // else if (a == b || b == c || a == c) {
+        //     return "isosceles";
+        // } 
+        // else {
+        //     return "scalene";
+        // }
     }
     public static void main(String args[]){
         int[] nums = {3,4,5};
