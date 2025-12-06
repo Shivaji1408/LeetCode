6834dcb13f9531dbc5787f16b71a8da98b21a0b0
Shiv Kumar <kumarshivaji0837@gmail.com>
2025-12-06 17:56:18 +0530

    Ques 1323 Solution

---
+++ Ques1323.java
@@ -0,0 +1,17 @@
+public class Ques1323 {
+    public static int maximum69Number (int num) {
+        char[] digits = String.valueOf(num).toCharArray();  
+        for (int i = 0; i < digits.length; i++) {
+            if (digits[i] == '6') {
+                digits[i] = '9';
+                break; 
+            }
+        }
+        return Integer.parseInt(new String(digits));
+    }
+
+    public static void main(String[] args) {
+        int num = 9669;
+        System.out.println(maximum69Number(num));
+    }
+}
