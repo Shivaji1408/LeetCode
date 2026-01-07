ba43c83e034ea8b62679178b3967209240a0904f
Shiv Kumar <kumarshivaji0837@gmail.com>
2026-01-07 23:54:10 +0530

    Ques1339 Solution

---
+++ Ques1339.java
@@ -0,0 +1,29 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Ques1339 {
+    private long ans = Long.MIN_VALUE;
+    private long total = 0;
+    private long dfs(TreeNode root) {
+        if (root == null) return 0;
+        long sum = root.val + dfs(root.left) + dfs(root.right);
+        ans = Math.max(ans, (total - sum) * sum);
+        return sum;
+    }
+    public int maxProduct(TreeNode root) {
+        total = dfs(root);
+        ans = Long.MIN_VALUE;
+        dfs(root);
+        return (int)(ans % 1000000007);
+    }
+}
