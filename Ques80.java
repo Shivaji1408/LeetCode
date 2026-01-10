e11a86229b9992ed9ce3fb25cdfdf724b05d53dd
Shiv Kumar <kumarshivaji0837@gmail.com>
2026-01-10 19:36:49 +0530

    Ques 80 Solution

---
+++ Ques80.java
@@ -0,0 +1,22 @@
+public class Ques80 {
+    public static int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        if(n<=2){
+            return n;
+        }
+
+        int j = 2;
+        for(int i=2; i<n; i++){
+            if(nums[i] != nums[j-2]){
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+
+    public static void main(String args[]){
+        int[] nums = {1,1,1,2,2,3};
+        System.out.println(removeDuplicates(nums));
+    }
+}
